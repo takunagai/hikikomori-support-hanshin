fd5bb189da3f5cc78c8865eb8f5bdac6fdb0d8ce
takunagai <tak_na@icloud.com>
2025-02-02 10:02:34 +0900

    chore: ESLint と Prettier をアップグレード\n\n- eslint を 9.19.0 に更新\n- eslint-config-next を 15.1.6 に更新\n- prettier を 3.4.2 に更新\n- prettier の設定ファイルを v3 形式に更新

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,10 @@
 // prettier-plugin-tailwind (prettier-plugin-svelte との併用は不可)
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
   trailingComma: "all",
   tabWidth: 2,
   semi: false,
-  plugins: [require("prettier-plugin-tailwindcss")],
-  // tailwindConfig: './styles/tailwind.config.js', // Default: 同ディレクトリの tailwind.config.js
+  plugins: ["prettier-plugin-tailwindcss"],
 }
+
+module.exports = config
